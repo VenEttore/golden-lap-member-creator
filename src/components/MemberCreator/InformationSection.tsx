68e62ab766bfd1035768295db903317cacccbf3b
mattadik123 <steven.aque@student.nmt.edu>
2025-06-06 20:27:58 -0600

    Continued refactor

---
+++ src/components/MemberCreator/InformationSection.tsx
@@ -0,0 +1,10 @@
+import { Card } from '../ui/Card';
+import React from 'react';
+
+export function InformationSection() {
+  return (
+    <Card title="Information">
+      <div />
+    </Card>
+  );
+} 
